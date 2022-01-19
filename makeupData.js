fb85f797a2b96804e725c69d2d977bb037805438
KrunalBorhade <krunalborhade2897@gmail.com>
2022-01-19 19:21:46 +0530

    Create makeupData.js
    Initial Commit

---
+++ makeupData.js
@@ -0,0 +1,3 @@
+var makeupData = JSON.parse(localStorage.getItem("makeupData"));
+var cartArr = JSON.parse(localStorage.getItem("cartItems")) || [];
+var wishlistArr = JSON.parse(localStorage.getItem("listItems")) || []
